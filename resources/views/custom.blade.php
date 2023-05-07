e5c263993b707404f317742f92b0e39d68024cbf
borongo <jfuentesverdugo@gmail.com>
2023-05-06 23:46:08 -0400

    curso 1

---
+++ resources/views/custom.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>custom</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A inventore eaque perferendis modi voluptatem commodi aspernatur earum assumenda facere at? Ullam, sequi ex hic vero est itaque totam laboriosam cupiditate.</p>
+</body>
+</html>
